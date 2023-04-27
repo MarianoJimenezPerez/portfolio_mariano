b51636423575dcb4ab2c818c7201a779c35af3ee
marianojimenezperez <marianojimenezperez@gmail.com>
2023-04-27 01:14:32 -0300

    Se agrega libreria typedjs y se modifica componente Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
+import Typed from 'typed.js';
+
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
+
 import ME from "./../assets/mariano-jimenez-perez.png";
 import CV from "./../assets/CV.pdf";
 
 const Hero = () => {
+  const el = React.useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['<h5 style="display:inline;">Front end developer</h5>', '<h5 style="display:inline;">Great person</h5>'],
+      typeSpeed: 70,
+      backSpeed: 30,
+      loop: true
+    });
+  
+    return () => {
+      typed.destroy();
+    }
+  }, [])
+  
   return (
     <div className="hero">
       <div className="container hero__container">
         <h5>Hello I'm</h5>
         <h1>Mariano Jiménez Pérez</h1>
-        <h5 className="text__secondary">Front end Developer</h5>
+        <span ref={el} className="text__secondary">
+          Front end
+        </span>
         <div className="cta">
           <a href={CV} download className="btn">
             Download CV
           </a>
-          <a href="#" className="btn btn__primary">
+          <a href="#contact" className="btn btn__primary">
             Let's talk
           </a>
         </div>
